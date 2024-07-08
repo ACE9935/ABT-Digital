8ecd51225b4cbb750fdd1418454156fa10025ff5
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-07-08 21:15:20 +0200

    commit

--- src/components/dashboad/layout/SideBar.tsx
+++ src/components/dashboad/layout/SideBar.tsx
@@ -1,7 +1,32 @@
-import { configurations } from "@/app-configurations";
+
 import Logo from "@/components/Logo";
 import SideBarLink from "./SideBarLink";
 import UserTab from "./UserTab";
+import { Home, NotificationAddOutlined, People, QrCode } from "@mui/icons-material";
+
+const links=[
+  {
+    title:"Acceuil",
+    url:"/dashboard",
+    icon:Home
+   },
+  {
+  title:"Qr Codes",
+  url:"/dashboard/qrcodes",
+  icon:QrCode
+ },
+   {
+    title:"Clients",
+    url:"/dashboard/clients",
+    icon:People
+   },
+   {
+    title:"Automatisation",
+    url:"/dashboard/send-requests",
+    icon:NotificationAddOutlined
+   }
+   ,
+]
 
 function SideBar() {
     return ( 
@@ -9,7 +34,7 @@ function SideBar() {
        <div className="bg-primary-color w-[17rem] sticky top-0 min-h-screen flex flex-col justify-between">
         <div className='flex flex-col gap-8 py-8'>
         <div className='flex pl-10'><Logo/></div>
-        <div className="flex flex-col pl-2 pr-4 gap-2">{configurations.dashBoardLinks.map((o,i)=><SideBarLink key={i} selected={false} {...o}/>)}</div>
+        <div className="flex flex-col pl-2 pr-4 gap-2">{links.map((o,i)=><SideBarLink key={i} selected={false} {...o}/>)}</div>
         
         </div>
         <div><UserTab/></div>
--- src/components/dashboad/layout/SideBarLink.tsx
+++ src/components/dashboad/layout/SideBarLink.tsx
@@ -1,7 +1,8 @@
+"use client"
 
-import { SvgIconComponent } from "@mui/icons-material";
 import { SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
+import { useRouter } from "next/navigation";
 
 type SideBarIconType =OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
     muiName: string;
@@ -16,8 +17,10 @@ interface SideBarItemProps {
 
 function SideBarLink({ url, title, selected,...props }: SideBarItemProps) {
     
+    const router=useRouter()
+
     return ( 
-        <div className={`py-3 text-white font-semibold hover:bg-black/40 rounded-xl w-full flex gap-3 cursor-pointer hover:text-primary-blue transition-all w-fit`}>
+        <div onClick={()=>router.push(url)} className={`py-3 text-white font-semibold hover:bg-black/40 rounded-xl w-full flex gap-3 cursor-pointer hover:text-primary-blue transition-all`}>
         <div className={`w-1 mr-4 ${selected?"bg-primary-blue":""}`}></div>
         <div className={`p-[0.4rem] ${selected?"border-[0.1rem] border-title-secondary rounded-full":""}`}><props.icon className={selected?"text-primary-blue":""}/></div>
         <span className="flex items-center">{title}</span>
